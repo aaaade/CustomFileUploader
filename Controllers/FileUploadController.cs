765bfbc23a1e0ac3beba03f37343d1f823b14d14
Adefolarin Adeniji <adefolarina@wakanow.com>
2016-11-10 11:46:26 +0100

    FInal Commit

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,107 @@
+﻿using DataModel;
+using jQuery_File_Upload.MVC5.Helpers;
+using jQuery_File_Upload.MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace jQuery_File_Upload.MVC5.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        FilesHelper filesHelper;
+        String tempPath = "~/somefiles/";
+        String serverMapPath = "~/Files/somefiles/";
+        private string StorageRoot
+        {
+            get { return Path.Combine(HostingEnvironment.MapPath(serverMapPath)); }
+        }
+        private string UrlBase = "/Files/somefiles/";
+        String DeleteURL = "/FileUpload/DeleteFile/?file=";
+        String DeleteType = "GET";
+        public FileUploadController()
+        {
+           filesHelper = new FilesHelper(DeleteURL, DeleteType, StorageRoot, UrlBase, tempPath, serverMapPath);
+        }
+      
+        public ActionResult Index(string id = "")
+        {
+            // set id if empty
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                id = DateTime.Now.ToString("ddMMMyyyyhhmmss");
+            }
+
+            // pass identifier to view
+            ViewBag.Id = id;
+
+            return View();
+        }
+        public ActionResult Show()
+        {
+            JsonFiles ListOfFiles = filesHelper.GetFileList("");
+            var model = new FilesViewModel()
+            {
+                Files = ListOfFiles.files
+            };
+          
+            return View(model);
+        }
+
+        public ActionResult Edit(string id = "")
+        {
+
+            ViewBag.Id = id;
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Upload()
+        {
+            var resultList = new List<ViewDataUploadFilesResult>();
+           
+            var CurrentContext = HttpContext;
+            string idd = Request.Form["idd"];
+
+            filesHelper.UploadAndShowResults(CurrentContext, resultList, idd);
+            JsonFiles files = new JsonFiles(resultList);
+
+            bool isEmpty = !resultList.Any();
+            if (isEmpty)
+            {
+                return Json("Error ");
+            }
+            else
+            {
+                return Json(files);
+            }
+        }
+        public JsonResult GetFileList(string id = "")
+        {
+
+            // set id if empty
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                id = DateTime.Now.ToString("ddMMMyyyyhhmmss");
+            }
+
+            // pass identifier to view
+            ViewBag.Id = id;
+
+            var list=filesHelper.GetFileList(id);
+            return Json(list,JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult DeleteFile(string file)
+        {
+            filesHelper.DeleteFile(file);
+            return Json("OK", JsonRequestBehavior.AllowGet);
+        }
+       
+    }
+}
---
+++ Models/fg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jQuery_File_Upload.MVC5.Models
+{
+    public static class fg
+    {
+        public static string GenerateUniqueCode()
+        {
+            return DateTime.Now.ToString("ddMMMyyyyhhmmss");
+        }
+    }
+}
